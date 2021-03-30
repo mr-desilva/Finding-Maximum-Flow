11b8e3f1c2e28c91f79212169ca31f00f6ecc4be
Tharindu <andrewdesilvasl@gmail.com>
2021-03-30 20:27:28 +0530

    added benchmark option

--- src/FindMaxFlow.java
+++ src/FindMaxFlow.java
@@ -1,18 +1,29 @@
+/*Student ID : w1761890
+* Student Name : Tharindu De Silva
+* */
+
+
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
 public class FindMaxFlow {
-    private int source = 0;
-    private int vertexCount;
-    private int sink = vertexCount - 1;
+    private int source = 0; // Store the source
+    private int vertexCount; // Store the number of nodes
+    private int sink = vertexCount - 1; // Store the target
     private Scanner sc = new Scanner(System.in);
     private Graph graph;
+
+    public static void main(String[] args) {
+        FindMaxFlow findMaxFlow = new FindMaxFlow();
+        findMaxFlow.execute();
+    }
 
     private static boolean bfs(Graph residualGraph, int source, int sink, int[] parent) {
         boolean[] visited = new boolean[residualGraph.getVertexCount()]; // Array to store the visited vertices
         LinkedList<Integer> queue = new LinkedList<>(); //queue which stores the vertices to be visited in order
-        for (int i = 0; i < residualGraph.getVertexCount(); i++)
+        for (int i = 0; i < residualGraph.getVertexCount(); i++) // initializing residual graph vertices as not visited
             visited[i] = false;
 
 
@@ -36,8 +47,57 @@ public class FindMaxFlow {
         }
         return visited[sink];
     }
-
+    // This is for benchmark without augmenting path for console
     private static int FordFulkerson(Graph graph, int source, int sink) {
+        int vertexCount = graph.getVertexCount(); //number of vertices
+        int[] parent = new int[vertexCount]; //store path with parent of each node
+        int maxFlow = 0; // max flow value
+
+        //error validation
+        if (source == sink) {
+            return 0;
+        }
+
+        // create residual graph
+        Graph residualGraph = new Graph(vertexCount);
+        for (int i = 0; i < vertexCount; i++) {
+            for (int j = 0; j < vertexCount; j++) {
+                residualGraph.getAdjacent()[i][j] = graph.getAdjacent()[i][j]; //map the original graph to the residual graph
+            }
+        }
+
+        // while a path exists from source to dest loop
+        while (bfs(residualGraph, source, sink, parent)) {
+            int pathFlow = Integer.MAX_VALUE; // to store path flow
+            ArrayList<Integer> pathView = new ArrayList<>(); //stores the augmenting path
+
+            // find maximum flow of path augmented by bfs method
+            for (int i = sink; i != source; i = parent[i]) {
+                pathView.add(i); //add visited node to the path
+                int j = parent[i];
+                pathFlow = Math.min(pathFlow, residualGraph.getAdjacent()[j][i]);
+            }
+
+            Collections.reverse(pathView);
+            pathView.clear();
+
+            // update residual graph capacities
+            // reverse edges along the path
+            for (int i = sink; i != source; i = parent[i]) {
+                int j = parent[i];
+                residualGraph.getAdjacent()[j][i] -= pathFlow;
+                residualGraph.getAdjacent()[i][j] += pathFlow;
+            }
+
+            // Add path flow to max flow
+            maxFlow += pathFlow;
+        }
+
+        return maxFlow;
+    }
+
+    // This for view the results with augmenting paths
+    private static int FordFulkersonWithPath(Graph graph, int source, int sink) {
         int vertexCount = graph.getVertexCount(); //number of vertices
         int[] parent = new int[vertexCount]; //store path with parent of each node
         int maxFlow = 0; // max flow value
@@ -98,7 +158,6 @@ public class FindMaxFlow {
                     vertexCount = Integer.parseInt(details[0]);
                     sink = vertexCount - 1;
                     graph = new Graph(vertexCount);
-                    System.out.println("Vertex count : " + vertexCount + " | Sink : " + sink);
                 } else {
                     String[] details = line.split(" ");
                     int node1 = Integer.parseInt(details[0]);
@@ -115,19 +174,25 @@ public class FindMaxFlow {
         }
     }
 
-    public static void main(String[] args) {
-        FindMaxFlow findMaxFlow = new FindMaxFlow();
-        findMaxFlow.execute();
-    }
-
-    public void findMaxFlow (String filename) {
+    public void benchMark(String filename) {
         long start = System.currentTimeMillis();
-       // String fileName_bridge1 = "datasets/bridge_1.txt";
         readFile(filename);
-        System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+        int maxFlow = FordFulkerson(graph, source, sink);
         long now = System.currentTimeMillis();
         double elapsed = (now - start) / 1000.0;
-        System.out.println(">>> Elapsed time = " + elapsed + " seconds <<<\n");
+        System.out.printf("%-18s %-17s %-11s %-14s %-11s %-23s",filename.replace("datasets/",""), graph.getVertexCount(), source, sink, maxFlow, elapsed);
+        System.out.println("");
+    }
+
+    public void findMaxFlow(String filename) {
+        long start = System.currentTimeMillis();
+        readFile(filename);
+        System.out.println("Number of Nodes : " + graph.getVertexCount() + "  ||  " + "Source : " + source + "  ||  " + "Sink : " + sink);
+        System.out.println("\nCalculated Max-Flow is: " + FordFulkersonWithPath(graph, source, sink) + "\n");
+        long now = System.currentTimeMillis();
+        double elapsed = (now - start) / 1000.0;
+        System.out.println("Elapsed time : " + elapsed);
+
     }
 
     private void execute() {
@@ -138,7 +203,8 @@ public class FindMaxFlow {
             System.out.println("===Select the option below to continue===");
             System.out.println("(1) To Calculate max flow in Bridge files");
             System.out.println("(2) To Calculate max flow in Ladder files");
-            System.out.println("(3) Exit the programme : ");
+            System.out.println("(3) To Calculate max flow in all files");
+            System.out.println("(4) Exit the programme : ");
             Scanner scanner = new Scanner(System.in);
             try {
                 int menuOption = scanner.nextInt();
@@ -159,62 +225,30 @@ public class FindMaxFlow {
                         switch (bridgeOption)
                         {
                             case 1:
-//                                long start = System.currentTimeMillis();
-//                                String fileName_bridge1 = "datasets/bridge_1.txt";
-//                                readFile(fileName_bridge1);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
-//                                long now = System.currentTimeMillis();
-//                                double elapsed = (now - start) / 1000.0;
-//                                System.out.println(">>> Elapsed time = " + elapsed + " seconds <<<\n");
                                 findMaxFlow("datasets/bridge_1.txt");
                                 break;
                             case 2:
-//                                long start = System.currentTimeMillis();
-//                                String fileName_bridge2 = "datasets/bridge_2.txt";
-//                                readFile(fileName_bridge2);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_2.txt");
                                 break;
                             case  3:
-//                                String fileName_bridge3 = "datasets/bridge_3.txt";
-//                                readFile(fileName_bridge3);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_3.txt");
                                 break;
                             case 4:
-//                                String fileName_bridge4 = "datasets/bridge_4.txt";
-//                                readFile(fileName_bridge4);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_4.txt");
                                 break;
                             case 5:
-//                                String fileName_bridge5 = "datasets/bridge_5.txt";
-//                                readFile(fileName_bridge5);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_5.txt");
                                 break;
                             case 6:
-//                                String fileName_bridge6 = "datasets/bridge_6.txt";
-//                                readFile(fileName_bridge6);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_6.txt");
                                 break;
                             case 7:
-//                                String fileName_bridge7 = "datasets/bridge_7.txt";
-//                                readFile(fileName_bridge7);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_7.txt");
                                 break;
                             case 8:
-//                                String fileName_bridge8 = "datasets/bridge_8.txt";
-//                                readFile(fileName_bridge8);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_8.txt");
                                 break;
                             case 9:
-//                                String fileName_bridge9 = "datasets/bridge_9.txt";
-//                                readFile(fileName_bridge9);
-//                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
                                 findMaxFlow("datasets/bridge_9.txt");
                                 break;
                         }
@@ -234,53 +268,57 @@ public class FindMaxFlow {
                         switch (ladderOption)
                         {
                             case 1:
-                                String fileName_ladder1 = "datasets/ladder_1.txt";
-                                readFile(fileName_ladder1);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_1.txt");
                                 break;
                             case 2:
-                                String fileName_ladder2 = "datasets/ladder_2.txt";
-                                readFile(fileName_ladder2);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_2.txt");
                                 break;
                             case  3:
-                                String fileName_ladder3 = "datasets/ladder_3.txt";
-                                readFile(fileName_ladder3);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_3.txt");
                                 break;
                             case 4:
-                                String fileName_ladder4 = "datasets/ladder_4.txt";
-                                readFile(fileName_ladder4);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_4.txt");
                                 break;
                             case 5:
-                                String fileName_ladder5 = "datasets/ladder_5.txt";
-                                readFile(fileName_ladder5);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_5.txt");
                                 break;
                             case 6:
-                                String fileName_ladder6 = "datasets/ladder_6.txt";
-                                readFile(fileName_ladder6);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_6.txt");
                                 break;
                             case 7:
-                                String fileName_ladder7 = "datasets/ladder_7.txt";
-                                readFile(fileName_ladder7);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_7.txt");
                                 break;
                             case 8:
-                                String fileName_ladder8 = "datasets/ladder_8.txt";
-                                readFile(fileName_ladder8);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_8.txt");
                                 break;
                             case 9:
-                                String fileName_ladder9 = "datasets/ladder_9.txt";
-                                readFile(fileName_ladder9);
-                                System.out.println("\nCalculated Max-Flow is: " + FordFulkerson(graph, source, sink) + "\n");
+                                findMaxFlow("datasets/ladder_9.txt");
                                 break;
                         }
                         break;
                     case 3:
+                        System.out.println("Filename\t\t" + "Num of Nodes\t\t" + "Source\t\t" + "Sink\t\t" + "Max flow\t\t" + "Time");
+                        benchMark("datasets/bridge_1.txt");
+                        benchMark("datasets/bridge_2.txt");
+                        benchMark("datasets/bridge_3.txt");
+                        benchMark("datasets/bridge_4.txt");
+                        benchMark("datasets/bridge_5.txt");
+                        benchMark("datasets/bridge_6.txt");
+                        benchMark("datasets/bridge_7.txt");
+                        benchMark("datasets/bridge_8.txt");
+                        benchMark("datasets/bridge_9.txt");
+                        benchMark("datasets/ladder_1.txt");
+                        benchMark("datasets/ladder_2.txt");
+                        benchMark("datasets/ladder_3.txt");
+                        benchMark("datasets/ladder_4.txt");
+                        benchMark("datasets/ladder_5.txt");
+                        benchMark("datasets/ladder_6.txt");
+                        benchMark("datasets/ladder_7.txt");
+                        benchMark("datasets/ladder_8.txt");
+                        benchMark("datasets/ladder_9.txt");
+                        break;
+
+                    case 4:
                         System.out.println("Exiting the programme......");
                         break exitMenu;
 
